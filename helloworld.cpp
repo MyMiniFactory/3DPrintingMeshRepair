3ebdb082684b648aede48755341d0b4684ef3355
tiger <ynotstartups@gmail.com>
2018-02-15 14:07:27 +0000

    working version

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,23 @@
 #include <vcg/complex/complex.h>
 #include <vcg/complex/algorithms/clean.h>
 #include <wrap/io_trimesh/import_stl.h>
+#include <vcg/complex/algorithms/inertia.h>
 
-class MyVertex; class MyEdge; class MyFace;
+class MyVertex; class MyFace;
 struct MyUsedTypes : public vcg::UsedTypes<vcg::Use<MyVertex>   ::AsVertexType,
-                            vcg::Use<MyEdge>     ::AsEdgeType,
                             vcg::Use<MyFace>     ::AsFaceType>{};
-class MyVertex  : public vcg::Vertex< MyUsedTypes, vcg::vertex::Coord3f, vcg::vertex::Normal3f, vcg::vertex::BitFlags  >{};
-class MyFace    : public vcg::Face<   MyUsedTypes, vcg::face::FFAdj,  vcg::face::VertexRef, vcg::face::BitFlags > {};
-class MyEdge    : public vcg::Edge<   MyUsedTypes> {};
-class MyMesh    : public vcg::tri::TriMesh< std::vector<MyVertex>, std::vector<MyFace> , std::vector<MyEdge>  > {};
+class MyVertex  : public vcg::Vertex< MyUsedTypes,
+    vcg::vertex::Coord3f,
+    vcg::vertex::Normal3f,
+    vcg::vertex::Mark,
+    vcg::vertex::BitFlags  >{};
+
+class MyFace    : public vcg::Face< MyUsedTypes,
+    vcg::face::FFAdj,
+    vcg::face::Normal3f,
+    vcg::face::VertexRef,
+    vcg::face::BitFlags > {};
+class MyMesh    : public vcg::tri::TriMesh< std::vector<MyVertex>, std::vector<MyFace> > {};
 
 int main( int argc, char **argv )
 {
@@ -30,11 +38,28 @@ int main( int argc, char **argv )
   // printf("Input mesh  vn:%i fn:%i\n",m.VN(),m.FN());
   printf( "Mesh has %i vert and %i faces\n", m.VN(), m.FN() );
 
-  vcg::tri::Clean<MyMesh>::RemoveDuplicateVertex(m);
-  printf( "after remove Duplicate Mesh has %i vert and %i faces\n", m.VN(), m.FN() );
 
-  // int edgeManifNum = vcg::tri::Clean<CMeshO>::CountNonManifoldEdgeFF(m,true);
-  // printf( "Nonmanifold edges number %i vert and %i faces\n", m.VN(), m.FN() );
+  bool RemoveDegenerateFlag=true;
+  vcg::tri::Clean<MyMesh>::RemoveDuplicateVertex(m, RemoveDegenerateFlag); // remove degenerateFace, removeDegenerateEdge, RemoveDuplicateEdge
+  printf( "after remove Duplicate Vertex has %i vert and %i faces\n", m.VN(), m.FN() );
+
+  bool NumDuplicateFaces = vcg::tri::Clean<MyMesh>::RemoveDuplicateFace(m);
+  printf( "removed %i duplicate faces\n", NumDuplicateFaces );
+  printf( "after remove Duplicate faces has %i vert and %i faces\n", m.VN(), m.FN() );
+
+  vcg::tri::UpdateTopology<MyMesh>::FaceFace(m);
+  // int edgeNonManifNum = vcg::tri::Clean<MyMesh>::CountNonManifoldEdgeFF(m,true);
+  // printf( "number of NonManif edges %i \n", edgeNonManifNum );
+
+  printf( "IsWaterTight %s \n", vcg::tri::Clean<MyMesh>::IsWaterTight(m) ? "True" : "False");
+  printf( "IsCoherentlyOrientedMesh %s \n", vcg::tri::Clean<MyMesh>::IsCoherentlyOrientedMesh(m) ? "True" : "False");
+
+  vcg::tri::Inertia<MyMesh> Ib(m);
+
+  printf( "Volume %f \n",Ib.Mass());
+
+  std::vector<MyFace *> IntersFaces;
+  vcg::tri::Clean<MyMesh>::SelfIntersections(m, IntersFaces);
 
   return 0;
 }
